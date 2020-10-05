afe9b10644a03fd94c872c7a39b8ffb4eb8cbba1
zach <zachcwilliams@icloud.com>
2020-10-04 18:37:42 -0700

    right nav icons, hover effect, and spacing

--- src/components/rightnav.jsx
+++ src/components/rightnav.jsx
@@ -9,9 +9,14 @@ import '../scss/rightnav.scss';
 import SearchIcon from "@material-ui/icons/Search";
 import FavoriteIcon from "@material-ui/icons/Favorite";
 import ForumIcon from "@material-ui/icons/Forum";
+import NotificationsActiveIcon from "@material-ui/icons/NotificationsActive";
+import FastfoodIcon from "@material-ui/icons/Fastfood";
 
 
 export default function RightNav() {
+
+  let searchIcon = <SearchIcon />
+
   return (
     <Router>
       <div className="right-nav-container">
@@ -22,23 +27,39 @@ export default function RightNav() {
               <input
                 className="rightnav-input"
                 type="text"
-                placeholder="Search Informance"
+                placeholder={`${searchIcon} Search Informance`}
               />
             </div>
-            <Link className="side-rightnav-link" to="/groups">
-              {/* <PersonIcon fontSize="large" className="nav-icon" /> */}
-              {/* <ForumIcon /> */}
-              <p className="side-rightnav-li">Community</p>
-            </Link>
-            <Link className="side-rightnav-link" to="/dating">
-              {/* <PersonIcon fontSize="large" className="nav-icon" /> */}
-              {/* <FavoriteIcon /> */}
-              <p className="side-rightnav-li">Dating</p>
-            </Link>
-            {/* <Link className="side-nav-link" to="/profile">
-              <PersonIcon fontSize="large" className="nav-icon" />
-              <p className="side-nav-li">Dating</p>
-            </Link> */}
+            <div className="rightnav-middle-container">
+              <h2 className="community-h2">Community</h2>
+              <Link className="side-rightnav-link" to="/groups">
+                <ForumIcon className="right-nav-icon" />
+                <p className="side-rightnav-li">Groups</p>
+              </Link>
+              <Link className="side-rightnav-link" to="/dating">
+                <FavoriteIcon className="right-nav-icon" />
+                <p className="side-rightnav-li">Dating</p>
+              </Link>
+              <Link className="side-rightnav-link" to="/notifs">
+                <NotificationsActiveIcon className="right-nav-icon" />
+                <p className="side-rightnav-li">Notifications</p>
+              </Link>
+              <Link className="side-rightnav-link" to="/messages">
+                <FastfoodIcon className="right-nav-icon" />
+                <p className="side-rightnav-li">Messages</p>
+              </Link>
+            </div>
+            <div className="pc-compass-container">
+              {/* <h2 className="community-h2">Take Political DNA Test</h2> */}
+              {/* <button className="pc-button">
+                <h2 className="community-h2">Poltical DNA</h2>
+                <img
+                  src="https://i.imgur.com/stD0fhI.png"
+                  alt=""
+                  className="political-compass"
+                />
+              </button> */}
+            </div>
           </ul>
         </nav>
 
